be2c59eb3b42d43363167f40416396b4b13b22b5
bess <1159066+bessx@users.noreply.github.com>
2026-03-02 04:13:12 -0800

    Commit prompt toast fix (#62)
    * remove Committed change toast
    
    * fix commit-prompt tests after removing toast

--- src/commands/commit-prompt.ts
+++ src/commands/commit-prompt.ts
@@ -29,7 +29,6 @@ export async function commitPromptCommand(scmProvider: JjScmProvider, jj: JjServ
 
     try {
         await withDelayedProgress('Committing...', jj.commit(message));
-        vscode.window.showInformationMessage('Committed change');
         await scmProvider.refresh({ reason: 'after commit' });
     } catch (err: unknown) {
         showJjError(err, 'Error committing change', scmProvider.outputChannel);
